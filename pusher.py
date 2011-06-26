5b3c59750d21be223a2d433decad4033ddb70ca3
Jesse D Zbikowski <embeddedlinuxguy@gmail.com>
2011-06-26 03:52:00 -0700

    Fix json

--- pusher.py
+++ pusher.py
@@ -10,13 +10,12 @@ def main():
     db = MySQLdb.connect(host="localhost", user=USER,
                   passwd=PASS,db="firebrowse")  
     c = db.cursor()
-    c.execute("""SELECT tweet FROM tweet LIMIT 100""")
+#    c.execute("""SELECT MAX(id) AS id FROM tweet""")
+    c.execute("""SELECT tweet FROM tweet LIMIT 10""")
     print """Content-type: text/json
 
 """
-    for i in range(10):
-        v = c.fetchone()
-        print v[0].strip()
+    print '[' + ', '.join(c.fetchone()[0].strip() for i in range(10)) + ']'
     
 if __name__ == '__main__':
     main()
