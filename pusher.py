138cbb0e00b88601cd18b97c2303ea152a922aab
Jesse D Zbikowski <embeddedlinuxguy@gmail.com>
2011-06-26 04:00:04 -0700

    Up to 100

--- pusher.py
+++ pusher.py
@@ -12,13 +12,13 @@ def main():
     c = db.cursor()
     c.execute("""SELECT MAX(id) AS id FROM tweet""")
     theid = c.fetchone()[0]
-    theid -= 10
+    theid -= 100
 
-    c.execute("""SELECT tweet FROM tweet WHERE id >= %s LIMIT 10""", (theid,))
+    c.execute("""SELECT tweet FROM tweet WHERE id >= %s LIMIT 100""", (theid,))
     print """Content-type: text/json
 
 """
-    print '[' + ', '.join(c.fetchone()[0].strip() for i in range(10)) + ']'
+    print '[' + ', '.join(c.fetchone()[0].strip() for i in range(100)) + ']'
     
 if __name__ == '__main__':
     main()
