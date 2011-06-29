40fd83540def034fb08ad956b5195aba2f2746cd
Jesse D Zbikowski <embeddedlinuxguy@gmail.com>
2011-06-29 00:04:43 -0700

    Get rid of old rows

--- grabber.py
+++ grabber.py
@@ -17,6 +17,8 @@ class Client:
     self.db = MySQLdb.connect(host="localhost", user=USER,
                   passwd=PASS,db="firebrowse")  
     self.c = self.db.cursor()
+    self.max = 1000
+    self.min = 1000
 
     self.conn = pycurl.Curl()  
     self.conn.setopt(pycurl.USERPWD, "%s:%s" % (USER, PASS))  
@@ -28,17 +30,22 @@ class Client:
     print "Connection done."
 
   def on_receive(self, data):  
-    self.buffer += data  
+    self.buffer += data
     if data.endswith("\r\n") and self.buffer.strip():
       self.c.execute("""INSERT INTO tweet (tweet) VALUES (%s)""", (self.buffer,))
 #      content = json.loads(self.buffer)  
       self.buffer = ""  
-      self.count = self.count + 1
-      if self.count % 100 == 0:
-          print self.count
-#      if self.count > 0:
-#          sys.exit(0)
-  
+      self.inserts += 1
+      if self.inserts > self.max:
+          self.prune()
+          self.inserts = 0
+
+  def prune(self):
+    self.c.execute("""SELECT COUNT(*) FROM tweet""")
+    count = c.fetchone()[0]
+    if count > self.max + self.min:
+      c.execute("""DELETE FROM tweet ORDER BY cur_timestamp LIMIT %s""", (count-self.min,))
+
 def main():
     client = Client()  
     # ? not reached ?
